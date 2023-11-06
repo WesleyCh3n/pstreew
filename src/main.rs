193287f6f7ba40e86d6ea85fe721376c51ff8f68
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2023-11-06 16:46:13 +0800

    feat: add process args and path

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use pstreew::PrintConfig;
 
 mod pstreew;
 
@@ -13,20 +14,26 @@ struct Args {
     parent: bool,
     #[arg(short = 'n', long, action)]
     sort: bool,
+    #[arg(short = 'd', long, action, default_value_t = false)]
+    hide_arg: bool,
+    #[arg(short = 'l', long, action, default_value_t = false)]
+    show_full_path: bool,
 }
 
 fn main() {
     let args = Args::parse();
+    let cfg = PrintConfig::new()
+        .show_pid(args.show_pid)
+        .sort(args.sort)
+        .show_arg(!args.hide_arg)
+        .show_path(args.show_full_path)
+        .build();
     if let Some(pid) = args.pid {
         if let Some(node) = pstreew::find_by_pid(pid) {
             if args.parent {
-                pstreew::print(
-                    pstreew::add_parent(node),
-                    args.show_pid,
-                    args.sort,
-                );
+                pstreew::print(pstreew::add_parent(node), &cfg);
             } else {
-                pstreew::print(node, args.show_pid, args.sort);
+                pstreew::print(node, &cfg);
             }
         }
         return;
@@ -34,6 +41,6 @@ fn main() {
 
     let trees = pstreew::list_all();
     for tree in trees {
-        pstreew::print(tree, args.show_pid, args.sort);
+        pstreew::print(tree, &cfg);
     }
 }
--- src/pstreew.rs
+++ src/pstreew.rs
@@ -3,48 +3,113 @@ use std::collections::{HashMap, HashSet, VecDeque};
 use std::cell::RefCell;
 use std::rc::Rc;
 
-use windows::Win32::System::Diagnostics::ToolHelp::{
-    CreateToolhelp32Snapshot, Process32First, Process32Next, PROCESSENTRY32,
-    TH32CS_SNAPPROCESS,
+use windows::Wdk::System::Threading::{
+    NtQueryInformationProcess, PROCESSINFOCLASS,
 };
+
+use windows::core::{HRESULT, PSTR, PWSTR};
+use windows::Win32::System::{
+    Diagnostics::{
+        Debug::ReadProcessMemory,
+        ToolHelp::{
+            CreateToolhelp32Snapshot, Process32First, Process32Next,
+            PROCESSENTRY32, TH32CS_SNAPPROCESS,
+        },
+    },
+    Threading::{
+        OpenProcess, PEB, PROCESS_BASIC_INFORMATION, PROCESS_QUERY_INFORMATION,
+        PROCESS_VM_READ, RTL_USER_PROCESS_PARAMETERS,
+    },
+};
+
+use colored::*;
+
+#[derive(Debug, Default)]
+pub struct ProcessInfo {
+    name: String,
+    path: String,
+    arg: String,
+}
 
 #[derive(Debug, Default)]
 pub struct PidNode {
     pid: u32,
-    name: String,
+    // name: String,
+    process_info: ProcessInfo,
     children: Vec<Rc<RefCell<PidNode>>>,
 }
 
 impl PidNode {
-    fn new(pid: u32, name: String) -> PidNode {
+    fn new(pid: u32, process_info: ProcessInfo) -> PidNode {
         PidNode {
             pid,
-            name,
+            process_info,
             children: Vec::new(),
         }
     }
 }
 
-pub fn print(root: Rc<RefCell<PidNode>>, show_pid: bool, sort: bool) {
-    dfs(root, Vec::new(), false, show_pid, sort);
+#[derive(Debug, Default)]
+pub struct PrintConfig {
+    show_pid: bool,
+    show_arg: bool,
+    show_path: bool,
+    sort: bool,
+}
+
+impl PrintConfig {
+    pub fn new() -> Self {
+        Default::default()
+    }
+    pub fn show_pid(mut self, show_pid: bool) -> Self {
+        self.show_pid = show_pid;
+        self
+    }
+    pub fn show_arg(mut self, show_arg: bool) -> Self {
+        self.show_arg = show_arg;
+        self
+    }
+    pub fn show_path(mut self, show_path: bool) -> Self {
+        self.show_path = show_path;
+        self
+    }
+    pub fn sort(mut self, sort: bool) -> Self {
+        self.sort = sort;
+        self
+    }
+    pub fn build(self) -> Self {
+        self
+    }
+}
+
+pub fn print(root: Rc<RefCell<PidNode>>, cfg: &PrintConfig) {
+    dfs(root, Vec::new(), false, cfg);
 }
 
 fn dfs(
     root: Rc<RefCell<PidNode>>,
     mut prefix: Vec<char>,
     have_siblings: bool,
-    show_pid: bool,
-    sort: bool,
+    cfg: &PrintConfig,
 ) {
     println!(
-        "{}{} {}",
+        "{}{} {} {}",
         prefix.iter().collect::<String>(),
-        if show_pid {
-            format!("({})", root.borrow().pid)
+        if cfg.show_pid {
+            format!("({})", root.borrow().pid).green()
         } else {
-            "".to_string()
+            ColoredString::default()
         },
-        root.borrow().name
+        if cfg.show_path {
+            root.borrow().process_info.path.clone()
+        } else {
+            root.borrow().process_info.name.clone()
+        },
+        if cfg.show_arg {
+            root.borrow().process_info.arg.dimmed()
+        } else {
+            ColoredString::default()
+        }
     );
     {
         let children = &root.borrow_mut().children;
@@ -63,7 +128,7 @@ fn dfs(
             prefix.append(&mut vec![' ', '╠', '═', ' ']);
         }
     }
-    if sort {
+    if cfg.sort {
         root.borrow_mut()
             .children
             .sort_by(|a, b| a.borrow().pid.cmp(&b.borrow().pid));
@@ -74,7 +139,7 @@ fn dfs(
             prefix.truncate(prefix.len() - 4);
             prefix.append(&mut vec![' ', '╚', '═', ' ']);
         }
-        dfs(child.clone(), prefix.clone(), !last_child, show_pid, sort);
+        dfs(child.clone(), prefix.clone(), !last_child, cfg);
     }
 }
 
@@ -126,9 +191,14 @@ pub fn add_parent(root: Rc<RefCell<PidNode>>) -> Rc<RefCell<PidNode>> {
             let pid = head.borrow().pid;
             if let Some(parent_pid) = mappid.get(&pid) {
                 if let Some(parent_name) = mapname.get(parent_pid) {
+                    let process_info = ProcessInfo {
+                        name: parent_name.clone(),
+                        ..Default::default()
+                    };
+
                     let new_head = Rc::new(RefCell::new(PidNode::new(
                         *parent_pid,
-                        parent_name.clone(),
+                        process_info,
                     )));
                     new_head.borrow_mut().children = vec![Rc::clone(&head)];
                     head = new_head;
@@ -155,9 +225,17 @@ pub fn find_by_pid(pid: u32) -> Option<Rc<RefCell<PidNode>>> {
         Process32First(snapshot, &mut entry as *mut PROCESSENTRY32).unwrap();
         loop {
             if entry.th32ProcessID == pid {
+                let mut process_info = ProcessInfo {
+                    name: PSTR::from_raw(entry.szExeFile.as_mut_ptr())
+                        .to_string()
+                        .unwrap(),
+                    ..Default::default()
+                };
+                get_proc_info(pid, &mut process_info).unwrap();
+
                 tree = Some(Rc::new(RefCell::new(PidNode::new(
                     pid,
-                    str_from_u8(&entry.szExeFile),
+                    process_info,
                 ))));
                 break;
             }
@@ -176,11 +254,15 @@ pub fn find_by_pid(pid: u32) -> Option<Rc<RefCell<PidNode>>> {
                 if let Some(node) =
                     find_child(parent.clone(), entry.th32ParentProcessID)
                 {
+                    let mut process_info = ProcessInfo {
+                        name: PSTR::from_raw(entry.szExeFile.as_mut_ptr())
+                            .to_string()
+                            .unwrap(),
+                        ..Default::default()
+                    };
+                    get_proc_info(pid, &mut process_info).unwrap();
                     node.borrow_mut().children.push(Rc::new(RefCell::new(
-                        PidNode::new(
-                            entry.th32ProcessID,
-                            str_from_u8(&entry.szExeFile),
-                        ),
+                        PidNode::new(entry.th32ProcessID, process_info),
                     )));
                 }
                 if Process32Next(snapshot, &mut entry as *mut PROCESSENTRY32)
@@ -232,6 +314,94 @@ pub fn list_all() -> Vec<Rc<RefCell<PidNode>>> {
     processes
 }
 
+pub fn get_proc_info(
+    pid: u32,
+    process_info: &mut ProcessInfo,
+) -> windows::core::Result<()> {
+    unsafe {
+        if pid == 0 {
+            return Ok(());
+        }
+        let h_process = match OpenProcess(
+            PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,
+            false,
+            pid,
+        ) {
+            Ok(h) => h,
+            Err(e) => {
+                if e.code() == HRESULT(0x80070005u32 as i32) {
+                    // Access is denied.
+                    process_info.path = e.message().to_string();
+                    process_info.arg = e.message().to_string();
+                    return Ok(());
+                }
+                return Err(e);
+            }
+        };
+        // get process basic information
+        let pic = std::mem::zeroed::<PROCESSINFOCLASS>();
+        let mut pbi =
+            std::mem::MaybeUninit::<PROCESS_BASIC_INFORMATION>::uninit();
+        NtQueryInformationProcess(
+            h_process,
+            pic,
+            pbi.as_mut_ptr() as *mut std::ffi::c_void,
+            std::mem::size_of::<PROCESS_BASIC_INFORMATION>() as u32,
+            std::ptr::null_mut(),
+        )
+        .unwrap();
+        // get peb
+        let pbi = pbi.assume_init();
+        let mut peb = std::mem::MaybeUninit::<PEB>::uninit();
+        ReadProcessMemory(
+            h_process,
+            pbi.PebBaseAddress as *const std::ffi::c_void,
+            peb.as_mut_ptr() as *mut std::ffi::c_void,
+            std::mem::size_of::<PEB>(),
+            Some(std::ptr::null_mut()),
+        )
+        .unwrap();
+        // get process parameters
+        let peb = peb.assume_init();
+        let mut proc_params =
+            std::mem::MaybeUninit::<RTL_USER_PROCESS_PARAMETERS>::uninit();
+        ReadProcessMemory(
+            h_process,
+            peb.ProcessParameters as *const std::ffi::c_void,
+            proc_params.as_mut_ptr() as *mut std::ffi::c_void,
+            std::mem::size_of::<RTL_USER_PROCESS_PARAMETERS>(),
+            Some(std::ptr::null_mut()),
+        )
+        .unwrap();
+        // get command line
+        let proc_params = proc_params.assume_init();
+        let cmdline_len = proc_params.CommandLine.Length as usize;
+        let mut cmdline: Vec<u16> = vec![0; cmdline_len];
+        ReadProcessMemory(
+            h_process,
+            proc_params.CommandLine.Buffer.as_ptr() as _,
+            cmdline.as_mut_ptr() as *mut std::ffi::c_void,
+            cmdline_len,
+            Some(std::ptr::null_mut()),
+        )
+        .unwrap();
+        process_info.arg = PWSTR::from_raw(cmdline.as_mut_ptr()).to_string()?;
+        // get image path
+        let img_path_len = proc_params.ImagePathName.Length as usize;
+        let mut img_path: Vec<u16> = vec![0; img_path_len];
+        ReadProcessMemory(
+            h_process,
+            proc_params.ImagePathName.Buffer.as_ptr() as _,
+            img_path.as_mut_ptr() as *mut std::ffi::c_void,
+            img_path_len,
+            Some(std::ptr::null_mut()),
+        )?;
+        process_info.path =
+            PWSTR::from_raw(img_path.as_mut_ptr()).to_string()?;
+        Ok(())
+    }
+}
+
 fn str_from_u8(bytes: &[u8]) -> String {
     std::str::from_utf8(
         &bytes[0..bytes
@@ -250,8 +420,13 @@ mod tests {
     #[test]
     fn find_test() {
         let tree = find_by_pid(7980);
+        let cfg = PrintConfig::new()
+            .show_pid(true)
+            .sort(false)
+            .show_arg(true)
+            .build();
         if let Some(tree) = tree {
-            print(tree, true, false);
+            print(tree, &cfg);
         }
     }
 
@@ -259,8 +434,13 @@ mod tests {
     fn list_test() {
         let processes = list_all();
         println!("{: <6}  Process Name", "PID");
+        let cfg = PrintConfig::new()
+            .show_pid(true)
+            .sort(true)
+            .show_arg(true)
+            .build();
         for process in processes {
-            print(process, true, false);
+            print(process, &cfg);
         }
     }
 }
